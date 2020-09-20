ed5ff27671ea149691a64f9d5329331240eb75bb
Reetik <reetik.raj.chy17@itbhu.ac.in>
2020-09-21 03:08:11 +0530

    added all the basic crud functionalities

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,23 @@ class PostsController < ApplicationController
         @post = Post.create(post_params)
         redirect_to posts_path
     end
+    def show
+        @post = Post.find_by_id(params[:id])
+    end
+    def update
+  		@post = Post.find_by_id(params[:id])
+  		@post.update(post_params)
+  		redirect_to(post_path(@post))
+	end
+    def edit
+        @post = Post.find_by_id(params[:id])
+	end
+	def destroy
+  		@post = Post.find_by_id(params[:id])
+  		@post.destroy
+  		redirect_to posts_path
+	end
+
     private
 
     def post_params
