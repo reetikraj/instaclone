a39d878bf534d58aca0e2645f4dafbc4cd2e68b3
Reetik <reetik.raj.chy17@itbhu.ac.in>
2020-09-21 03:33:19 +0530

    reduced code redundancy to follow the Dry principle

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+	before_action :set_post, only: [:show, :edit, :update, :destroy]
+
 	def index
         @posts = Post.all
     end
@@ -10,24 +12,27 @@ class PostsController < ApplicationController
         redirect_to posts_path
     end
     def show
-        @post = Post.find_by_id(params[:id])
+        
     end
     def update
-  		@post = Post.find_by_id(params[:id])
+  		
   		@post.update(post_params)
-  		redirect_to(post_path(@post))
+  		redirect_to posts_path
 	end
     def edit
-        @post = Post.find_by_id(params[:id])
-	end
+        
+   	end
 	def destroy
-  		@post = Post.find_by_id(params[:id])
+  		
   		@post.destroy
   		redirect_to posts_path
 	end
 
     private
-
+    
+    def set_post
+    	@post = Post.find_by_id(params[:id])
+  	end
     def post_params
         params.require(:post).permit(:image, :caption)
     end
